9bf3287870509b616015078323b50625402160fd
Cristian <ruanoperezcristian10@gmail.com>
2024-10-30 09:26:57 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,65 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function App() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 ..."
+      style={{
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh'
+      }}>
+      <div style={{
+        display: 'flex', 
+        flexDirection: 'row', 
+        padding: '2rem', 
+        borderRadius: '16px', 
+        backgroundColor: 'black', 
+        boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)', 
+        maxWidth: '700px', 
+        width: '90%',  
+        height: 'auto', 
+        flexWrap: 'wrap', 
+        alignItems: 'center', 
+      }}>
+        
+        <div style={{ flexShrink: 0, flexBasis: '300px', maxWidth: '100%', display: 'flex', justifyContent: 'flex-start' }}>
+          <img
+            alt="Card background"
+            style={{ 
+              objectFit: 'cover', 
+              borderRadius: '16px', 
+              width: '90%',  
+              height: '300px', 
+              marginLeft: '-60px', 
+              marginTop: '-10px', 
+            }} 
+            src="Imagee.jpg"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div style={{ 
+          paddingLeft: '1rem', 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'flex-start',
+          color: 'white', 
+          flex: '1' 
+        }}>
+          <p style={{ fontSize: '2rem', fontWeight: 'bold', margin: '1rem 0 0' }}>Jess Wilson</p>
+          <small style={{ color: '#ddd', fontSize: '1rem' }}>UX Engineer</small>
+          <small style={{ color: '#ddd', fontSize: '1rem' }}>Empowering users through captivating interfaces, turning ideas into pixel-perfect realities.</small>
+
+          <div style={{ display: 'flex', marginTop: '1rem', gap: '1rem' }}>
+            <button style={{backgroundColor: 'black', color: 'white', border: '2px solid white',borderRadius: '20px', padding: '1rem 1.5rem', cursor: 'pointer'
+            }}>
+              PROFILE
+            </button>
+            <button style={{backgroundColor: 'white', color: 'black', border: 'none',borderRadius: '20px', padding: '1rem 1.5rem', cursor: 'pointer'
+            }}>
+              FOLLOW
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
